bb309474b61570eac6a29b864ad49dc2b6961484
Nechami Feld <nechamifeld@gmail.com>
2023-03-14 11:48:20 +0200

    new changes

---
+++ PicturePuzzle/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace PicturePuzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::PicturePuzzle.Properties.Resources.shutterstock_175583699;
+            this.pictureBox4.Location = new System.Drawing.Point(653, 330);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(135, 135);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::PicturePuzzle.Properties.Resources.shutterstock_188749790;
+            this.pictureBox3.Location = new System.Drawing.Point(653, 189);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(135, 135);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PicturePuzzle.Properties.Resources.Baby_Virginia_Opossum;
+            this.pictureBox1.Location = new System.Drawing.Point(653, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(135, 135);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(653, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "import picture";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(653, 471);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(135, 135);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 686);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "help";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 657);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "print functions";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 696);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(219, 729);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(345, 729);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "undo";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 812);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ PicturePuzzle/Form1.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PicturePuzzle
+{
+    
+
+    public partial class Form1 : Form
+    {
+        int[,] mat = new int[5, 5];
+        myPictureBox[,] myPictureBoxArray = new myPictureBox[5, 5];
+        int x = 0, y = 4;
+        myPictureBox whitePictureBox;
+        Image image;
+        int width, height;
+        Panel p;
+        Bitmap bitMap;
+
+        string status = "user";//system-user
+
+        stackFunctions stack;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            stack = new stackFunctions();
+
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                    if (i == 0 && j == 4)  mat[i, j] = 0;
+                    else
+                        mat[i, j] = 1;
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            image = ((PictureBox)sender).Image;
+
+            CreatePuzzle(image);
+ 
+        }
+
+        private void picturebox_Click(object sender, EventArgs e)
+        {
+            //if (stack.isEmpty())
+            //{
+            //    you finished the game
+            //    MessageBox.Show("you finished th game");
+            //}
+            moveControl(((myPictureBox)sender));
+        }
+
+        private void CreatePuzzle(Image image)
+        {
+            bitMap = new Bitmap(image);
+            width = bitMap.Width;
+            height = bitMap.Height;
+
+            createPanel();
+
+            seperateImage();
+
+            mixPicture();
+
+        }
+
+        private void mixPicture()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 150; i++)
+            {
+                int direction = rnd.Next(4);
+
+                switch (direction)
+                {
+                    case 0: if (whitePictureBox.Y > 0) moveControl(myPictureBoxArray[whitePictureBox.X, whitePictureBox.Y - 1]); break;//up
+                    case 1: if (whitePictureBox.Y < 4) moveControl(myPictureBoxArray[whitePictureBox.X, whitePictureBox.Y + 1]); break;//down
+                    case 2: if (whitePictureBox.X > 0) moveControl(myPictureBoxArray[whitePictureBox.X - 1, whitePictureBox.Y]); break;//right
+                    case 3: if (whitePictureBox.X < 4) moveControl(myPictureBoxArray[whitePictureBox.X + 1, whitePictureBox.Y]); break;//left
+                    default:
+                        break;
+                }
+            }
+
+
+            
+        }
+
+        private void moveControl(myPictureBox pictureBox)
+        {
+            int Xcolor = pictureBox.X;
+            int Ycolor = pictureBox.Y;
+            int Xwhite = whitePictureBox.X;
+            int Ywhite = whitePictureBox.Y;
+
+            if (status == "user")
+            {
+                if (Xcolor == Xwhite && Ycolor < Ywhite)
+                    stack.push(Direction.down);
+                if (Xcolor == Xwhite && Ycolor > Ywhite)
+                    stack.push(Direction.up);
+                if (Xcolor > Xwhite && Ycolor == Ywhite)
+                    stack.push(Direction.left);
+                if (Xcolor < Xwhite && Ycolor == Ywhite)
+                    stack.push(Direction.right);
+            }
+            
+
+
+            int temp;
+            myPictureBox tempPicturebox;
+            Point location = pictureBox.Location;
+            Size size = pictureBox.Size;
+            int i = location.X / 120;
+            int j = location.Y / 120;
+
+            if ((i + 1 == x && j == y) || (i - 1 == x && j == y) || (i == x && j == y + 1) || (i == x && j == y - 1))
+            {
+                mat[i, j] = 0;
+                mat[x, y] = 1;
+
+
+                p.Controls.Remove(pictureBox);
+                p.Controls.Remove(whitePictureBox);
+
+
+                temp = Xcolor;
+                pictureBox.X = Xwhite;
+                whitePictureBox.X = temp;
+
+                temp = Ycolor;
+                pictureBox.Y = Ywhite;
+                whitePictureBox.Y = temp;
+
+
+                tempPicturebox = new myPictureBox(p.Width, p.Height);
+                tempPicturebox.X = whitePictureBox.X;
+                tempPicturebox.Y = whitePictureBox.Y;
+               
+                tempPicturebox.Location = location;
+                tempPicturebox.BackColor = Color.White;
+                p.Controls.Add(tempPicturebox);
+                whitePictureBox = tempPicturebox;
+
+
+                tempPicturebox = new myPictureBox(p.Width, p.Height, x, y);
+                tempPicturebox.X = pictureBox.X;
+                tempPicturebox.Y = pictureBox.Y;
+                
+                tempPicturebox.Click += picturebox_Click;
+               
+                tempPicturebox.Image = pictureBox.Image;
+                p.Controls.Add(tempPicturebox);
+                pictureBox = tempPicturebox;
+
+
+                x = i;
+                y = j;
+
+
+                myPictureBoxArray[Xcolor, Ycolor] = whitePictureBox;
+                myPictureBoxArray[Xwhite, Ywhite] = pictureBox;
+            }
+
+           
+        }
+
+        private void seperateImage()
+        {
+            myPictureBox picturebox;
+            Bitmap map;
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                {
+                    myPictureBoxArray[i, j] = new myPictureBox();
+                    if (i == 0 && j == 4)
+                    {
+                        picturebox = new myPictureBox(p.Width, p.Height, 0, 4);
+                        picturebox.BackColor = Color.White;
+                        p.Controls.Add(picturebox);
+
+                        picturebox.X = i;
+                        picturebox.Y = j;
+
+                        whitePictureBox = picturebox;
+                        myPictureBoxArray[i, j] = picturebox;
+                    }
+                    else
+                    {
+                        picturebox = new myPictureBox(p.Width, p.Height, i, j);
+                        picturebox.Click += picturebox_Click;
+                        p.Controls.Add(picturebox);
+
+
+                        map = new Bitmap(bitMap.Width / 5, bitMap.Height / 5);
+                        for (int k = 0; k < map.Width; k++)
+                            for (int l = 0; l < map.Height; l++)
+                            {
+                                map.SetPixel(k, l, bitMap.GetPixel(i * (bitMap.Width / 5) + k, j * (bitMap.Height / 5) + l));
+                            }
+                        picturebox.Image = map;
+                        picturebox.X = i;
+                        picturebox.Y = j;
+
+                        if (i > 0) picturebox.validFunctions.Add(Direction.right);
+                        if (i < 4) picturebox.validFunctions.Add(Direction.left);
+                        if (j > 0) picturebox.validFunctions.Add(Direction.up);
+                        if (j < 4) picturebox.validFunctions.Add(Direction.down);
+
+                        myPictureBoxArray[i, j] = picturebox;
+
+                    }
+
+                }
+        }
+
+        private void createPanel()
+        {
+            p = new Panel();
+            p.Location = new Point(20, 20);
+            p.Size = new System.Drawing.Size(600, 600);
+            p.BorderStyle = BorderStyle.FixedSingle;
+
+
+            Controls.Add(p);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+
+            
+            pictureBox2.Image = new Bitmap(file.FileName);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(stack.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            numericUpDown1.Visible = true;
+            button4.Visible = true;
+
+            numericUpDown1.Minimum = 1;
+            numericUpDown1.Maximum = stack.Count;
+
+            label1.Text = "there's " + stack.Count.ToString() + " last functions. how many functions do you want to undo?";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            status = "system";
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                Direction? direction = stack.pop();
+                switch (direction)
+                {
+                    case Direction.up: moveControl(myPictureBoxArray[whitePictureBox.X, whitePictureBox.Y - 1]) ; break;
+                    case Direction.down: moveControl(myPictureBoxArray[whitePictureBox.X, whitePictureBox.Y + 1]);break;
+                    case Direction.right: moveControl(myPictureBoxArray[whitePictureBox.X + 1, whitePictureBox.Y]);break;
+                    case Direction.left: moveControl(myPictureBoxArray[whitePictureBox.X - 1, whitePictureBox.Y]);break;
+                    default:
+                        break;
+                }
+            }
+            status = "user";
+            label1.Text = "there's " + stack.Count.ToString() + " last functions. how many functions do you want to undo?";
+        }
+
+
+    }
+}
+
+
+class myPictureBox:PictureBox
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+  
+    
+
+    public List<Direction> validFunctions { get; set; }
+
+    public myPictureBox(int Width, int Height,int i,int j)
+        : base()
+    {
+
+        validFunctions = new List<Direction>();
+
+
+        SizeMode = PictureBoxSizeMode.StretchImage;
+        BorderStyle = BorderStyle.FixedSingle;
+        Size = new Size(Width / 5, Height / 5);
+        Location = new Point(i * (Width / 5), j * (Height / 5));
+    }
+
+
+    public myPictureBox(int Width, int Height)
+        : base()
+    {
+
+        validFunctions = new List<Direction>();
+
+        SizeMode = PictureBoxSizeMode.StretchImage;
+        BorderStyle = BorderStyle.FixedSingle;
+        Size = new Size(Width / 5, Height / 5);
+    }
+
+    public myPictureBox():base()
+    {
+        validFunctions = new List<Direction>();
+    }
+}
+
+
+enum Direction
+{
+    up, down, right, left
+}
+
+
+class stackFunctions 
+{
+    private List<Direction> list;
+
+    public int Count
+    {
+        get { return list.Count ; }
+    }
+
+    public override string ToString()
+    {
+        string s = string.Empty;
+        for (int i = 0; i < list.Count; i++)
+        {
+            s += list[i].ToString() + "\t";
+        }
+        return s;
+    }
+
+    public void push(Direction direction)
+    {
+        Direction? direct = top();
+        if (direct != null)
+        {
+            if ((direction == Direction.up && direct == Direction.down) ||
+                (direction == Direction.down && direct == Direction.up) ||
+                (direction == Direction.right && direct == Direction.left) ||
+                (direction == Direction.left && direct == Direction.right))
+                pop();
+            else
+                list.Add(direction);
+        }
+        else list.Add(direction);
+
+    }
+
+    public bool isEmpty()
+    {
+        return Count == 0;
+    }
+
+    public Direction? top()
+    {
+        if (list.Count != 0) return list.Last();
+        return null;
+    }
+
+    public Direction? pop()
+    {
+        Direction? direction = top();
+        if (list.Count != 0)  
+            list.RemoveAt(list.Count() - 1);
+        return direction;
+    }
+
+    public stackFunctions()
+    {
+        list = new List<Direction>();
+    }
+}
